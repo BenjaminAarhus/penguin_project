9ba09191e510bcaa6f09677d0d0a83ce1728dd6e
BenjaminAarhus <benjaminaarhus@yahoo.co.uk>
2025-09-24 14:14:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+title: "README"
+output: html_document
+---
+
+Penguin_Data project using data from three different species of penguins to explore relationships between their morphometric traits
---
+++ penguin.R
@@ -0,0 +1,24 @@
+# Install and load tidyverse packages
+install.packages("tidyverse")
+library(tidyverse)
+
+# Load penguin data from penguin_data.txt
+read.table("data/penguin_data.txt", header = T)
+glimpse(penguins)
+
+# Run a linear regression on the penguin data
+penguinLm <- lm(body_mass ~ flipper_len, data = penguins)
+summary(penguinLm)
+
+# Save figure of the last plot ran 
+ggsave("figs/1_flipper_bodymass_regression.png")
+
+# Create a subset of the data to look at sex
+penguinFemale <- subset(penguins, sex == "female")
+
+# Save the edited data set
+write_tsv(penguinFemale, "results/1_penguin_female_only.txt")
+
+
+
+
